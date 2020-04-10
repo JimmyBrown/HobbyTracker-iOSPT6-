a4e894bedcfc70c5cb839362efd3f536ec790e79
Jimmy Brown <jabro4@gmail.com>
2020-04-09 19:29:21 -0700

    Add tableview

--- HobbyTracker(iOSPT6)/FriendsTableViewController.swift
+++ HobbyTracker(iOSPT6)/FriendsTableViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  FriendsTableViewController.swift
 //  HobbyTracker(iOSPT6)
 //
 //  Created by Jimmy Brown on 4/9/20.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FriendsTableViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
